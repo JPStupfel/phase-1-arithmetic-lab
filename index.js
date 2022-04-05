91c48e4da0deb5e62ede15e62b5a9aecbe220bde
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-05 12:23:56 -0500

    commit tree a31061895c21683ff0a0b0dce40d0f8529aea27d

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 62 / num1
+let multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10)
+
+const num4 = 22
+const num3 = num4 + 4
+let mod = num3 % num4
+
+let max = Math.max(1,2,20)
+
+console.log(max)
